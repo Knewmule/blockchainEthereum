f5c19f5f28cc347493395664bc6d59197febabc1
bigproblems55 <pro@core>
2024-01-05 10:59:37 -0800

    sent a trasaction with notes on how to do so in truffle console

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,7 +2,11 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract faucet {
-    uint public funds = 1000;
-    int public counter = -10;
-    uint32 public test = 4294967295; 
+    // this is a special function
+    //its called when you make a tx that does not specify function name to call
+
+    // External function are part of the contract interface
+    // which means they can be called via contracts and other tx
+    
+    receive() external payable {}
 }
--- truffle-config.js
+++ truffle-config.js
@@ -20,7 +20,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {                   // Is 0.5.1 in video was 0.8.21 it changed to 5 1 works
-      version: "0.5.1",      // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.4",      // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
